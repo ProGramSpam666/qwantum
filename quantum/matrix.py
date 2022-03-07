c12f4cfedd894a2f6b08e6680307b845954a6fdc
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2022-03-07 12:05:53 +0000

    matrix.py utils.py

---
+++ quantum/matrix.py
@@ -0,0 +1,17 @@
+import numpy as np
+from utils import kinetic
+
+
+def fillmatrix(ik, N_G, N_k, potential):  # matrix we want to fill with Hamiltonian
+    a = potential.parms["lattice"]
+    M = np.zeros((2*N_G+1, 2*N_G+1), dtype=np.complex_)  # matrix to fill
+    for ig1 in range(-N_G, N_G+1):
+        for ig2 in range(-N_G, N_G+1):
+            if (ig1 == ig2):
+                M[ig1+N_G, ig2+N_G] = kinetic(ik, ig1, a, N_k)
+                #kinetic - Diagonal
+            else:
+                M[ig1+N_G, ig2+N_G] = potential.ft(ig2-ig1)
+                #potential - Outside
+    return M
+#No potential component for KE
---
+++ quantum/utils.py
@@ -0,0 +1,10 @@
+from math import pi
+
+def Gvec(m,a):
+        return 2*pi*m/a
+
+def kvec(i,a,N):
+        return -pi/a + i*2*pi/a/(N-1)
+
+def kinetic(i,m,a,N):
+        return 0.5*(kvec(i,a,N) - Gvec(m,a))**2 
