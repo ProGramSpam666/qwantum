ae029c25027aa76cbca01b7a80c1571b883ecee5
ProGramSpam666 <101066111+ProGramSpam666@users.noreply.github.com>
2022-03-07 18:34:45 +0000

    testingfors

--- test.py
+++ test.py
@@ -1,31 +1,53 @@
 from quantum.schrodinger import solveSchrodinger
 from quantum.schrodinger import solveSchroedinger1
 from quantum import potential as pt
-from testbasis import testckfunc, testckfunc2
+from testbasis import testckfunc, testckfunc2, testcktilda1, testcktildafinal, combineckfunc
+
 
 pf = pt.PotentialFactory()
 pf.addType("sech", pt.sechpotGenerator, pt.sechFTGenerator)
 ptparms = { "lattice" : 2, "depth" : 1, "width" :0.1 }
 ptl = pf.createPotential("sech", ptparms)
 
+
 ek,ck = solveSchrodinger(10,100,5,ptl)
 print("-----------EK-----------")
 print(ek.size)
+
+
 print("-----------CK-----------")
 print(ck.size)
+
+print("---------------TESTshapeck----------")
+print(ck.shape)
+
 print("----------TESTck[:, 0, i]--------")
-print(testckfunc(3, 50, 4, ptl))
+print(testckfunc(3, 50, 4, ptl)) #arguments = (N_G, N_k, N_b, potential)
+
 print("---------TESTck[:, i]----------")
 print(testckfunc2(3, 50, 4, ptl))
 
+print("--------TESTckcombine-------")
+print(combineckfunc(3, 50, 4, ptl))
+
+
+print("-------TESTckTilda[:, l, i]---------")
+ckTilda1 = testcktilda1(3, 50, 4, ptl)
+print(testcktilda1(3, 50, 4, ptl))
+print(ckTilda1.size)
+
+print("TESTcktilda[:, l , i]final--------")
+ckTildafinal = testcktildafinal(3, 50, 4, ptl)
+print(testcktildafinal(3, 50, 4, ptl))
+print(ckTildafinal.size)
+
+print("------TEST")
+print()
 
 
 
-#Take note how band structure and eigenenergies at the gamma point change when you fix the number of k-points (N_k)
-#and change the number of plane-waves (N_G dependent) in the basis set expansion ()
 
-#Fix plane-waves (N_G dependent) and vary the number of k-point (N_k) and check change of band structure (depending on ek) 
-# AND the eigenenergies (ek) at the gamma point
 
-#Investigate how specific number of plane waves (REMEMBER: up to cutoff N_G) used can 
- 
+
+
+
--- testbasis.py
+++ testbasis.py
@@ -18,17 +18,57 @@ def determineOptimalBasis(N_G, N_b, N_k, sb, ck, potential):
         optimalBasis = ckNEW
     return optimalBasis
 
+
+#checking form 
 def testckfunc(N_G, N_k, N_b, potential):
+    N = N_b
     ck = solveSchroedinger1(N_G,N_k,N_b,potential)
-    for i in range(N_b):
+    for i in range(N):
         new = ck[:, 0, i]
         return new   
 
 def testckfunc2(N_G, N_k, N_b, potential):
+    N = N_b
     ck = solveSchroedinger1(N_G,N_k,N_b,potential)
-    for i in range(N_b):
+    for i in range(N):
         new = ck[:, i]
-        return new       
+        return new
+
+#testing applicability of first for loop
+def combineckfunc(N_G, N_k, N_b, potential):
+    N = N_b
+    ck = solveSchroedinger1(N_G,N_k,N_b,potential)
+    for i in range(N):
+        print("-----------GOAT-----------")
+        print(ck[:, i].shape)
+        print(ck[:,0, i].shape)
+    return ck
+
+
+#checking cktilda
+def testcktilda1(N_G, N_k, N_b, potential):
+    ck = solveSchroedinger1(N_G,N_k,N_b,potential)
+    ckTilda = np.zeros(np.shape(ck), dtype= np.complex_)
+    for l in range(1, N_k):
+        for i in range(N_b):
+            ckTilda[:, l, i] = ck[:, l , i]
+    return ckTilda
+
+#
+def testcktildafinal(N_G, N_k, N_b, potential):
+    N = N_b
+    ck = solveSchroedinger1(N_G,N_k,N_b,potential)
+    OB_bi = np.zeros((np.shape(ck)[0], np.shape(ck)[1] * np.shape(ck)[2]), dtype= np.complex_)
+    ckTilda = np.zeros(np.shape(ck), dtype= np.complex_)
+    for l in range(1, N_k):
+        for i in range(N_b):
+            ckTilda[:, l, i] = ck[:, l , i]
+            for j in range(N):
+                ckTilda[:, l , i] -= OB_bi[:,j]*(np.dot(OB_bi[:,j], ck[:, l , i]))
+    return ckTilda
+
+#
+""" def testallck(N_G, N_k, N_b, potential):
 
 
 
@@ -36,17 +76,18 @@ def testckfunc2(N_G, N_k, N_b, potential):
 
 
 
+def testOBbi(N_G, N_k, N_b, potential):
 
 
 
+Np = N
+        for i in range(N_b):
+            for j in range(Np, N):
+                ckTilda[:, l, i] -= OB_bi[i, j]*(np.dot(OB_bi[:,j], ck[:, l , i]))
+                alpha = np.dot(ckTilda[:, l, i], ckTilda[:, l ,i])
+                if alpha >= sb:
+                    N += 1
+                    OB_bi[:,N] = ckTilda[:,l,i] / sqrt(alpha)
 
 
-
-
-
-
-
-
-
-
-
+ """
