4e962c7bf36d373b5c6f9ebf9b356d903cbd24f2
ProGramSpam666 <101066111+ProGramSpam666@users.noreply.github.com>
2022-03-07 16:32:26 +0000

    readmeupdate

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-from quantum.schrodinger import solveSchrodinger
+""" from quantum.schrodinger import solveSchrodinger
+from quantum.schrodinger import solveSchroedinger1
 from quantum import potential as pt
 
 pf = pt.PotentialFactory()
@@ -11,3 +12,15 @@ print("-----------EK-----------")
 print(ek.size)
 print("-----------CK-----------")
 print(ck.size)
+ """
+
+
+#Take note how band structure and eigenenergies at the gamma point change when you fix the number of k-points (N_k)
+#and change the number of plane-waves (N_G dependent) in the basis set expansion ()
+
+#Fix plane-waves (N_G dependent) and vary the number of k-point (N_k) and check change of band structure (depending on ek) 
+# AND the eigenenergies (ek) at the gamma point
+
+
+#Investigate how specific number of plane waves (REMEMBER: up to cutoff N_G) used can 
+ 
--- testbasis.py
+++ testbasis.py
@@ -4,18 +4,25 @@ from quantum.schrodinger import solveSchroedinger1
 from quantum.optimalbasis import OptimalBasis
 
 
-def determineBasis(N_G, N_b, N_k, sb, ck, potential):
+def determineBasis(N_G, N_b, N_k, ck, potential):
     [ek,ck] = solveSchrodinger(N_G,N_k,N_b,potential)
     for i in range(N_b):
         basis = ck
-        return basis
-        
+    return basis
+
+
 def determineOptimalBasis(N_G, N_b, N_k, sb, ck, potential):
-    [ek,ck] = solveSchrodinger(N_G,N_k,N_b,potential)
+    ck = solveSchrodinger(N_G,N_k,N_b,potential)
     ckNEW = OptimalBasis(sb, N_b, N_k, ck)
-    for i in range(N_b):
+    for sb in range(N_b):
         optimalBasis = ckNEW
-        return optimalBasis
+    return optimalBasis
+
+
+
+
+
+
 
 
 
