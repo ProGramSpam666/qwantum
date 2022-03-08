f21f919581315e1009e9fba3114bb53ebf764fc7
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2022-03-08 15:33:57 +0000

    devs

--- quantum/tests/potentialTester.py
+++ quantum/tests/potentialTester.py
@@ -1,6 +1,32 @@
-from quantum.potential import PotentialFactory
+
 
 class PotentialTester:
-    def __init__(self):
-        self.pf = PotentialFactory()
-        
+    ## CONSTRUCTOR
+    def __init__(self, ptparms):
+        self.ptparms = self.setPtparms(ptparms)
+        self.pf = pt.PotentialFactory()
+
+    ## GETTERS
+    def getPtparms(self):
+        return self.ptparms
+
+    ## SETTERS
+    def setPtparms(self, ptparms):
+        self.ptparms = ptparms
+    def addSechType(self):
+        self.pf.addType("sech", pt.sechpotGenerator, pt.sechFTGenerator)
+
+    ## GENERATORS
+    def generateSechPotential(self):
+        self.addSechType()
+        return self.pf.createPotential("sech", self.ptparms)
+
+
+
+ptparms = { "lattice" : 2, "depth" : 1, "width" :0.1 }
+potentialTester = PotentialTester(ptparms)
+sechPotential = potentialTester.generateSechPotential()
+print(sechPotential)
+
+
+    
