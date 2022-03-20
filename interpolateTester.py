03b447c22b572c622fbfe4075c16c465b50906a1
leninRessurect <74080985+leninRessurect@users.noreply.github.com>
2022-03-20 20:44:02 +0000

    k0 k1
    interpolate dev

--- interpolateTester.py
+++ interpolateTester.py
@@ -4,18 +4,54 @@ import quantum.qobj
 
 class InterpolateTester(unittest.TestCase):
 
-    qobj = quantum.qobj.Qobj()
-
-    def test_00_calculateVLoc(self):
+    """ def test_00_calculateVLoc(self):
         print("Start calculateVLoc test \n")
         qobj = quantum.qobj.Qobj()
         result = quantum.interpolate.calculateVLoc(
             OB_bi=qobj.getOptimalBasis(),
             N_G=qobj.getN_G(),
-            N_GPrime= 11,
-            N_GPrimePrime=23,
+            N_GPrime= 23,
+            N_GPrimePrime=79,
             potential=qobj.getPotential(),
-            Ncell=25,
-            Npoints=1000,
+            Ncell=3,
+            Npoints=10,
+        )
+        print(result) """
+
+    """ def test_01_calculateK0(self):
+        print("Start calculateK0 test \n")
+        qobj = quantum.qobj.Qobj()
+        res = quantum.interpolate.calculatek0(
+            OB_bi=qobj.getOptimalBasis(),
+            a = qobj.getPotential().parms["lattice"],
+            m = 10
+        )
+        print(res)
+
+    def test_02_calculateK1(self):
+        print("Start calculateK1 test \n")
+        qobj = quantum.qobj.Qobj()
+        res = quantum.interpolate.calculatek1(
+            OB_bi=qobj.getOptimalBasis(),
+            a = qobj.getPotential().parms["lattice"],
+            m = 10
+        )
+        print(res) """
+
+    def test_02_interpolateHamiltonian(self):
+        print("Start interpolateHamiltonian test \n")
+        qobj = quantum.qobj.Qobj()
+        result = quantum.interpolate.interpolateHamiltonian(
+            OB_bi=qobj.getOptimalBasis(),
+            N_G=qobj.getN_G(),
+            m=10,
+            N_GPrime=23,
+            N_GPrimePrime=79,
+            Ncell=3,
+            Npoints=10,
+            potential=qobj.getPotential(),
         )
         print(result)
+
+
+        
--- quantum/interpolate.py
+++ quantum/interpolate.py
@@ -3,57 +3,73 @@ from quantum.utils import Gvec
 
 #eq 16, obtain ek, compare with solveschrodinger
 
-def calculatek0(OB_bi, Nbasis, a):
+def calculatek0(OB_bi, m, a):
     k0 = np.zeros(np.shape(OB_bi), dtype = np.complex_)
-    for i in(Nbasis):
-        for j in(Nbasis):
-            for m in range(0, Gvec):
-                k0[i, j] = 2*(np.conjugate(OB_bi[m, i])*OB_bi[m, j]*Gvec(m, a))
+    
+    for i in range(OB_bi.shape[0]):
+        for j in range(OB_bi.shape[1]):
+            k0[i, j] = 2*(np.conjugate(OB_bi[m, i])*OB_bi[m, j]*Gvec(m, a))
 
     return k0
 
-def calculatek1(OB_bi, Nbasis, a):
+def calculatek1(OB_bi, m, a):
     k1 = np.zeros(np.shape(OB_bi), dtype = np.complex_)
-    for i in(Nbasis):
-        for j in(Nbasis):
-            for m in range(0, Gvec):
-                k1[i, j] = np.conjugate(OB_bi[m, i])*OB_bi[m, j]*(Gvec(m, a)**2)
+    for i in range(k1.shape[0]):
+        for j in range(k1.shape[1]):    
+            k1[i, j] = np.conjugate(OB_bi[m, i])*OB_bi[m, j]*(Gvec(m, a)**2)
 
     return k1
 
 
 def calculateVLoc(OB_bi, N_G, N_GPrime, N_GPrimePrime, potential, Ncell, Npoints):
     a = potential.parms["lattice"]
-    x= potential.v(Ncell, Npoints)
+    x = np.asarray(potential.v(Ncell, Npoints), dtype=np.complex_)
+    vloc = np.zeros(np.shape(x), dtype=np.complex_)
+    
     for item in range(0,N_G):
         for i in range(0, N_GPrime):
             for j in range(0, N_GPrimePrime):
                     summation = np.conjugate(OB_bi[item, i])*potential.ft(i-j)*OB_bi[item,j]
                     integralTerm = (np.exp(1j*Gvec(i,a))*x)*(np.exp(1j*Gvec(item, a))*x)*(np.exp(1j*Gvec(j, a))*x)
-                    vloc =vloc + summation*integralTerm
+                    term = summation*integralTerm
+                    vloc += term
+                    
     return vloc
 
 
-
-def interpolateHamiltonian(OB_bi, Nbasis, kList, potential, N_G, m):
-    vLoc = calculateVLoc()
+def interpolateHamiltonian(OB_bi, potential, N_G, N_GPrime, N_GPrimePrime, Ncell, Npoints, m):
+    vLoc = calculateVLoc(
+        OB_bi=OB_bi,
+        N_G=N_G,
+        N_GPrime=N_GPrime,
+        N_GPrimePrime=N_GPrimePrime,
+        potential=potential,
+        Ncell=Ncell,
+        Npoints=Npoints,
+    )
     a = potential.parms["lattice"]
+    k0 = calculatek0(
+        OB_bi=OB_bi,
+        m=m,
+        a=a
+    )
+    k1 = calculatek1(
+        OB_bi=OB_bi,
+        m=m,
+        a=a
+    )
+    M = np.zeros((2*N_G+1, 2*N_G+1), dtype=np.complex_)
     kList = 100
-    k0 = np.zeros(np.shape(OB_bi), dtype = np.complex_)
-    k1 = np.zeros(np.shape(OB_bi), dtype = np.complex_)
-    vLoc = np.zeros(np.shape(OB_bi), dtype = np.complex_)
-    for i in(Nbasis):
-        for j in(Nbasis):
-            for m in range(0, Gvec):
-                k0[i, j] += 2*(np.conjugate(OB_bi[m, i])*OB_bi[m, j]*Gvec(m, a))
-                k1[i, j] += np.conjugate(OB_bi[m, i])*OB_bi[m, j]*(Gvec(m, a)**2)
 
-            M = np.zeros((2*N_G+1, 2*N_G+1), dtype=np.complex_)
-            for k in(kList):
+    for i in range(OB_bi.shape[0]):
+        for j in range(OB_bi.shape[1]):
+            for k in range(kList):
+                print(k)
                 if (i == j):
-                    M[i+N_G, j+N_G] = 0.5*(k**2 + kList*k1[i,j] + k0[i,j])#check kronecker
+                    M[i+N_G, j+N_G] = 0.5*(k**2 + kList*k1[i,j] + k0[i,j])
                 else:
                     M[i+N_G, j+N_G] = vLoc[i,j]
+
     return M
 
 
